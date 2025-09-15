7403e8ab0408289292422259adea9a0fd92a4d5a
John Dyreby <john@isentropic.dev>
2025-09-15 14:04:26 -0500

    Update deps

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,9 @@
-const { defaultTheme } = require("@vuepress/theme-default");
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
+import { viteBundler } from "@vuepress/bundler-vite";
 
-module.exports = {
+export default defineUserConfig({
+  bundler: viteBundler(),
   theme: defaultTheme({
     logo: "/sketchbox_logo.png",
     logoDark: "/sketchbox_logo_dark_mode.png",
@@ -11,11 +14,12 @@ module.exports = {
       "/faq.md",
       "/troubleshooting.md",
       "/resources",
-      "/version-history"
+      "/version-history",
     ],
     contributors: false,
   }),
-  lang: 'en-US',
-  title: 'Sketchbox Documentation',
-  description: 'Documentation for Sketchbox, a simple building energy modeling tool, by Slipstream',
-};
+  lang: "en-US",
+  title: "Sketchbox Documentation",
+  description:
+    "Documentation for Sketchbox, a simple building energy modeling tool, by Slipstream",
+});
